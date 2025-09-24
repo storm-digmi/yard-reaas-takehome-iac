25838ec3f02edb3e0cddc48ff2620267de6883ea
Alessandro Pindozzi <a.pindozzi@STO31177.station>
2025-09-24 15:08:26 +0200

    test pipeline

--- layers/aws/orbital_freight_infra/variables.tf
+++ layers/aws/orbital_freight_infra/variables.tf
@@ -4,19 +4,16 @@
 
 variable "project_info" {
 description = "Project Info"
-type = string
 default = {}
 }
 
 variable "aws_region" {
 description = "AWS region"
-type = string
 default = {}
 }
 
 variable "lambda" {
 description = "Lambda Info"
-type = string
 default = {}
 }
 
